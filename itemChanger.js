1167f7897a975a3910d1c8b46b86869693e6bc13
Jan <jschne12@hs-mittweida.de>
2021-01-18 16:06:45 +0100

    Resolve design error

--- itemChanger.js
+++ itemChanger.js
@@ -39,6 +39,7 @@ function reset() {
     var items = document.getElementsByClassName("item")
     for(var i = 0; i < items.length; i++) {
         items[i].innerText = 'o'
+        items[i].classList.remove("edited")
     }
 }
 
