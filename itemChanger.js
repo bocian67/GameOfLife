46a77a762579beb2858e97cae05e88549cc125f9
Jan <jschne12@hs-mittweida.de>
2021-01-18 16:12:32 +0100

    Outsource some variables

--- itemChanger.js
+++ itemChanger.js
@@ -1,3 +1,5 @@
+const baseURL = "http://192.168.178.239:5000/board/"
+const length = 12
 function changeValue(e) {
     if(e.innerText == "o") {
         e.innerText = "x"
@@ -11,27 +13,27 @@ function changeValue(e) {
 
 function sendMatrixToPi() {
     var items = document.getElementsByClassName("item")
-    var valueArray = Array.from(Array(12), () => new Array(12))
+    var valueArray = Array.from(Array(length), () => new Array(length))
     for(var i = 0; i < items.length; i++) {
-        var quotient = Math.floor(i/12)
-        valueArray[quotient][i-(quotient*12)] = items[i].innerText
+        var quotient = Math.floor(i/length)
+        valueArray[quotient][i-(quotient*length)] = items[i].innerText
     }
     var json = JSON.stringify(valueArray)
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://192.168.178.239:5000/board/new", true);
+    xhttp.open("POST", baseURL + "new", true);
     xhttp.setRequestHeader("Content-Type", "application/json");
     xhttp.send(json)
 }
 
 function doRandom() {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://192.168.178.239:5000/board/random", true);
+    xhttp.open("POST", baseURL + "random", true);
     xhttp.send()
 }
 
 function sendTerminate() {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://192.168.178.239:5000/board/terminate", true);
+    xhttp.open("POST", baseURL + "terminate", true);
     xhttp.send()
 }
 
